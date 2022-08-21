cdd437889a2370817e52315cb18965c335ad7af5
FatimaAlmashhor <fatima.amin4official@gmail.com>
2022-08-21 22:40:27 +0300

    Final: cursor

--- src/js/index.js
+++ src/js/index.js
@@ -10,75 +10,13 @@ const lerp = (a, b, n) => (1 - n) * a + n * b;
 const elements = document.querySelectorAll('.link');
 // const cursor = document.querySelector('.cursor')
 
-
 // track the cursor postion
-// const cursor_position = { x: 0, y: 0 }
+cursor_position = { x: 0, y: 0 }
 
-// // render for all the need information
-// const render_info = {
-//     tx: { previous: 0, current: 0, amt: 0.2 },
-//     ty: { previous: 0, current: 0, amt: 0.2 },
-//     scale: { previous: 1, current: 1, amt: 0.2 },
-//     opacity: { previous: 1, current: 1, amt: 0.2 },
-// }
-
-
-
-
-
-// const onMouseMove = () => {
-
-//     const width = cursor.getBoundingClientRect().width / 2;
-//     const height = cursor.getBoundingClientRect().height / 2;
-
-//     render_info.tx.current = cursor_position.x - width;
-//     render_info.ty.current = cursor_position.y - height;
-
-//     requestAnimationFrame(() => render());
-
-//     window.removeEventListener('mousemove', onMouseMove);
-// }
-
-// const onMouseInter = () => {
-//     render_info.scale.current = 2;
-// }
-
-// const onMouseLeave = () => {
-//     render_info.scale.current = 1;
-// }
-
-// const render = () => {
-//     render_info.tx.previous = lerp(render_info.tx.previous, render_info.tx.current, render_info.tx.amt);
-//     render_info.ty.previous = lerp(render_info.ty.previous, render_info.ty.current, render_info.ty.amt);
-//     render_info.scale.previous = lerp(render_info.scale.previous, render_info.scale.current, render_info.scale.amt);
-
-//     cursor.style.display = 'block';
-//     cursor.style.transform = `translateX(${(render_info.tx.previous)}px) translateY(${render_info.ty.previous}px) scale(${render_info.scale.previous})`;
-//     // loop...
-//     requestAnimationFrame(() => render());
-// }
-
-// window.addEventListener('mousemove', (e) => {
-//     cursor_position.x = e.clientX;
-//     cursor_position.y = e.clientY;
-
-//     onMouseMove();
-// })
-
-// elements.forEach(e => {
-//     e.addEventListener('mouseenter', (event) => {
-//         onMouseInter();
-//         console.log('mouse inter');
-//     })
-
-//     e.addEventListener('mouseleave', event => {
-//         onMouseLeave()
-//         console.log('mouse leave');
-
-//     })
-// })
-
-
+window.addEventListener('mousemove', (e) => {
+    cursor_position.x = e.clientX;
+    cursor_position.y = e.clientY;
+})
 
 // this is the Cursor class
 
@@ -90,8 +28,7 @@ class Cursor {
         inner: null
     }
 
-    // track the cursor postion
-    cursor_position = { x: 0, y: 0 }
+
 
     // render for all the need information
     render_info = {
@@ -101,16 +38,24 @@ class Cursor {
         opacity: { previous: 1, current: 1, amt: 0.2 },
     }
 
+    width = 0;
+    height = 0;
     constructor(DOM) {
         this.DOM.el = DOM;
         this.DOM.inner = this.DOM.el.querySelector('.cursor__inner');
 
-        window.addEventListener('mousemove', (e) => {
-            this.cursor_position.x = e.clientX;
-            this.cursor_position.y = e.clientY;
+        this.width = this.DOM.el.getBoundingClientRect().width / 2;
+        this.height = this.DOM.el.getBoundingClientRect().height / 2;
 
-            this.onMouseMove();
-        })
+
+        // Check if any options passed in data attributes
+        this.scaleOnEnter = this.DOM.el.dataset.scaleEnter || this.scaleOnEnter;
+        this.opacityOnEnter = this.DOM.el.dataset.opacityEnter || this.opacityOnEnter;
+        for (const key in this.render_info) {
+            this.render_info[key].amt = this.DOM.el.dataset.amt || this.render_info[key].amt;
+        }
+
+        this.onMouseMove();
 
         elements.forEach(e => {
             e.addEventListener('mouseenter', (event) => {
@@ -122,16 +67,12 @@ class Cursor {
 
             })
         })
-
-
     }
 
     onMouseMove = () => {
-        const width = this.DOM.el.getBoundingClientRect().width / 2;
-        const height = this.DOM.el.getBoundingClientRect().height / 2;
 
-        this.render_info.tx.current = this.cursor_position.x - width;
-        this.render_info.ty.current = this.cursor_position.y - height;
+        this.render_info.tx.previous = this.render_info.tx.current = cursor_position.x - this.width;
+        this.render_info.ty.previous = this.render_info.ty.previous = cursor_position.y - this.height;
 
         requestAnimationFrame(() => this.render());
 
@@ -139,7 +80,7 @@ class Cursor {
     }
 
     onMouseInter = () => {
-        this.render_info.scale.current = 2;
+        this.render_info.scale.current = 3;
     }
 
     onMouseLeave = () => {
@@ -148,10 +89,12 @@ class Cursor {
 
     render = () => {
 
+        this.render_info.tx.current = cursor_position.x - this.width;
+        this.render_info.ty.current = cursor_position.y - this.height;
+
         for (const key in this.render_info) {
             this.render_info[key].previous = lerp(this.render_info[key].previous, this.render_info[key].current, this.render_info[key].amt);
         }
-
 
         this.DOM.el.style.display = 'block';
         this.DOM.el.style.transform = `translateX(${(this.render_info.tx.previous)}px) translateY(${this.render_info.ty.previous}px) scale(${this.render_info.scale.previous})`;
@@ -161,5 +104,7 @@ class Cursor {
 
 }
 
-
-new Cursor(document.querySelector('.cursor'));
+const cursers = document.querySelectorAll('.cursor');
+cursers.forEach(c => {
+    new Cursor(c);
+})
